0e64e41d08b34c8a4a1c8f600c45b5181f99f0d0
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2024-05-17 23:28:59 +0200

    bert model

--- gdpr/models/bert_model/bert.py
+++ gdpr/models/bert_model/bert.py
@@ -92,3 +92,51 @@ class TransformerEncoderBlock(nn.Module):
         x = self.mlp_block(x) + x
         return x
     
+
+class BERT(nn.Module):
+    def __init__(self,
+                 img_size:int=224, 
+                 in_channels:int=3,
+                 patch_size:int=16, 
+                 num_transformer_layers:int=12, 
+                 embedding_dim:int=768, 
+                 mlp_size:int=3072, 
+                 num_heads:int=12,
+                 attn_dropout:float=0,
+                 mlp_dropout:float=0.1, 
+                 embedding_dropout:float=0.1,
+                 num_classes:int=1000): 
+        super().__init__()
+
+
+
+        self.class_embedding = nn.Parameter(data=torch.randn(1, 1, embedding_dim),
+                                            requires_grad=True)
+
+        self.embedding_dropout = nn.Dropout(p=embedding_dropout)
+
+        self.embedding = Embedding(in_channels=in_channels,
+                                              patch_size=patch_size,
+                                              embedding_dim=embedding_dim)
+        
+        self.transformer_encoder = nn.Sequential(*[TransformerEncoderBlock(embedding_dim=embedding_dim,
+                                                                            num_heads=num_heads,
+                                                                            mlp_size=mlp_size,
+                                                                            mlp_dropout=mlp_dropout) for _ in range(num_transformer_layers)])
+
+        self.classifier = nn.Sequential(
+            nn.LayerNorm(normalized_shape=embedding_dim),
+            nn.Linear(in_features=embedding_dim,
+                      out_features=num_classes)
+        )
+
+    def forward(self, x):
+        batch_size = x.shape[0]
+
+        x = self.patch_embedding(x)
+
+        x = self.embedding_dropout(x)
+        x = self.transformer_encoder(x)
+        x = self.classifier(x)
+
+        return x
